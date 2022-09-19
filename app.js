c5c98e453b4cd931d8e5ddafbaadfb2f86f4964c
web-developer-ridoy <103379670+web-developer-ridoy@users.noreply.github.com>
2022-09-19 01:13:34 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+
+
+const input1 = document.getElementById("gaja");
+const input2 = document.getElementById("raja");
+const input3 = document.getElementById("taja");
+
+
+input3.onclick = ()=>{
+    console.log(input1.value);
+}
+
+input3.onclick = ()=>{
+    console.log(input2.value);
+}
